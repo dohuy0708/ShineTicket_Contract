d50d98673501769ddf64184612ed88ad5fbdc1ae
dohuy0708 <dohuy22314@gmail.com>
2026-06-19 22:57:23 +0700

    verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,8 @@
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-import { configVariable, defineConfig } from "hardhat/config";
 import * as dotenv from "dotenv";
 dotenv.config();
-export default defineConfig({
+
+export default {
   plugins: [hardhatToolboxMochaEthersPlugin],
   solidity: {
     profiles: {
@@ -28,26 +28,31 @@ export default defineConfig({
   },
   networks: {
     hardhat: { type: "edr-simulated", allowUnlimitedContractSize: true },
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
-      allowUnlimitedContractSize: true,
-    },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
-    sepolia: {
-      type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
-    },
     amoy: {
       type: "http",
-      url: process.env.RPC_URL || "https://rpc-amoy.polygon.technology", // Dùng Public RPC nếu không có Infura
+      url: process.env.RPC_URL || "https://rpc-amoy.polygon.technology",
       chainId: 80002,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
-});
+  etherscan: {
+    apiKey: {
+      polygonAmoy: "2M373VBQCAUPAFNYZZQ1X8637YHG28P26H",
+      polygonMumbai: "2M373VBQCAUPAFNYZZQ1X8637YHG28P26H",
+      polygon: "2M373VBQCAUPAFNYZZQ1X8637YHG28P26H",
+      polygonscan: "2M373VBQCAUPAFNYZZQ1X8637YHG28P26H",
+      polygonScan: "2M373VBQCAUPAFNYZZQ1X8637YHG28P26H",
+      amoy: "2M373VBQCAUPAFNYZZQ1X8637YHG28P26H"
+    },
+    customChains: [
+      {
+        network: "polygonAmoy",
+        chainId: 80002,
+        urls: {
+          apiURL: "https://api-amoy.polygonscan.com/api",
+          browserURL: "https://amoy.polygonscan.com/"
+        }
+      }
+    ]
+  }
+};
